d50950c7652a9ed7f1566a6960d483ec18eb2d5d
Wilmer Coronado <wilmer.coronado.mt@correo.usa.edu.co>
2021-11-09 22:08:52 +0100

    First commit

---
+++ src/main/java/Reto3_Cabin/Reto3_Cabin/InterfaceMensaje.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3_Cabin.Reto3_Cabin;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface InterfaceMensaje extends CrudRepository<Mensaje,Integer> {
+    
+}
